c2f6ccb412255a5576fe21e3fccfbfdc150604a9
Jorge Salinas <94187187+jorgesalinas07@users.noreply.github.com>
2024-02-03 12:39:53 -0500

    CIN-6-create-the-one-hundred-and-one-dalmatians-page (#8)
    feat: Implement the One Hundred and One Dalmatians page

--- javascript/get-movie.js
+++ javascript/get-movie.js
@@ -1,5 +1,7 @@
 import { OPTIONS, API_MOVIE_ID_URL } from "./constants.js";
 
+export const ONE_HUNDRED_AND_ONE_DALMATIANS_MOVIE_ID = 'tt0055254'
+
 export const get_movie = async(movieId) => {
   try {
     const apiUrl = API_MOVIE_ID_URL.replace('{id}', movieId);
